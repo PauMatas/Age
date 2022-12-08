b8eb03f57fca74118f6c00622a2edd2bf5901447
Pau Matas <pmatasalbiol@gmail.com>
2022-12-08 11:33:15 +0100

    fix: dates are 0-indexed

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-function yearsDifference(date1, date2, decimals=8) {
-    var difference = date1.getTime() - date2.getTime();
-    return (difference / 1000 / 60 / 60 / 24 / 365.25).toFixed(decimals);
+function yearsDifference(date1, date2, decimals = 8) {
+  var difference = date1.getTime() - date2.getTime();
+  return (difference / 1000 / 60 / 60 / 24 / 365.25).toFixed(decimals);
 }
 
 function age() {
-    const currentdate = new Date();
-    const birthdate = new Date(2001, 12, 7, 12, 30, 0, 0);
+  const currentdate = new Date();
+  // Be aware that the month is 0-indexed, so 11 is actually December
+  const birthdate = new Date(2001, 11, 7, 12, 30, 0, 0);
 
-    const realage = yearsDifference(currentdate, birthdate)
-    const integer = Math.floor(realage)
-    const decimal = Math.floor((realage - integer) * 10 ** 8)
+  const realage = yearsDifference(currentdate, birthdate);
+  const integer = Math.floor(realage);
+  const decimal = Math.floor((realage - integer) * 10 ** 8);
 
-    document.getElementById('integer').innerHTML = integer;
-    document.getElementById('decimal').innerHTML = `.${decimal}`;
+  document.getElementById("integer").innerHTML = integer;
+  document.getElementById("decimal").innerHTML = `.${decimal}`;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    age();
-    setInterval(age, 500);
+document.addEventListener("DOMContentLoaded", function () {
+  age();
+  setInterval(age, 500);
 });
